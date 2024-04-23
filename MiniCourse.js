06dbd9855cb128562e471d8e108af91b160e478a
Burke <02john.burke@gulllakecs.org>
2024-04-23 10:36:50 -0700

    MyMiniCourse.js part 2

---
+++ MiniCourse.js
@@ -0,0 +1,12 @@
+// Programmer: John Burke
+// Team Member: Jack Sutton
+// Bolt ID: SB E2
+// Date: 4.16.24
+// Program: Hello World - Sandbox
+// Program URL: https://edu.sphero.com/program/16743349/edit
+
+async function startProgram() {
+
+}
+
+
