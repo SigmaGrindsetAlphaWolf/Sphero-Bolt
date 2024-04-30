d675f1c20b2917410f734227984f50d2c4bcecf5
Burke <02john.burke@gulllakecs.org>
2024-04-30 11:05:02 -0700

    Template for competition

--- MazeCompition.js
+++ MazeCompition.js
@@ -6,5 +6,34 @@
 // Program URL: https://edu.sphero.com/program/16788347/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: John Burke - Start Text & Audtio Checkpoint Commit - Date:	
+	
+
+	
+	
+	// Programmer:             - Blue LED Checkpoint Commit - Date:
+	
+	
+	
+	// Programmer:             - Sound One Checkpoint Commit - Date:
+	
+	
+	
+	// Programmer:             - Red LED Checkpoint Commit - Date:
+	
+	
+	
+	// Programmer:             - Sound Two Checkpoint Commit - Date:	
+	
+	
+	
+	// Programmer:             - Green LED Checkpoint Commit - Date:
+	
+	
+	
+	// Programmer:             - Purple LED & Sound Three Checkpoint Commit - Date:
+	
+	
+	
+	// Programmer: Jack Sutton - Start Text & Audtio Checkpoint Commit - Date:	
 }
