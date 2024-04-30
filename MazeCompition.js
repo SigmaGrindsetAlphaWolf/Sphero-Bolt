24d7bf51433147f44a61e3f8430a76f83e33545b
Burke <02john.burke@gulllakecs.org>
2024-04-30 10:28:28 -0700

    Added begginning comments and started program and java script

---
+++ MazeCompition.js
@@ -0,0 +1,10 @@
+// Programmer: John Burke
+// Team Member: Jack Sutton
+// Bolt ID: SB E2
+// Date: 4.16.24
+// Program: Maze Competition
+// Program URL: https://edu.sphero.com/program/16788347/edit
+
+async function startProgram() {
+	// Write code here
+}
